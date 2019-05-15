585a1d7205e9dcc559e6ce9a96c35c105fa52aeb
sujini <tgif20146@gmail.com>
2019-05-15 17:11:43 +0900

    2019.05.15 Spring DispatcherServlet 작동과정 (init,service,doGet) spring-servlet.xml

---
+++ src/main/java/com/cafe24/springex/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.cafe24.springex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "/WEB-INF/views/hello.jsp";
+	}
+}
